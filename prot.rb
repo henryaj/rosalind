d26b4a8d1bfbe72fd2485520fb6ec9cce892c2c7
Henry Stanley <henry@henrystanley.com>
2015-10-29 23:11:07 +0000

    Add triplet to codon conversion

--- prot.rb
+++ prot.rb
@@ -2,4 +2,14 @@ CODONS = {
   "UUU" => "F", "UUC" => "F", "UUA" => "L", "UUG" => "L", "UCU" => "S", "UCC" => "S", "UCA" => "S", "UCG" => "S", "UAU" => "Y", "UAC" => "Y", "UGU" => "C", "UGC" => "C", "UGG" => "W", "GUU" => "V", "GUC" => "V", "GUA" => "V", "GUG" => "V", "GCU" => "A", "GCC" => "A", "GCA" => "A", "GCG" => "A", "GAU" => "D", "GAC" => "D", "GAA" => "E", "GAG" => "E", "GGU" => "G", "GGC" => "G", "GGA" => "G", "GGG" => "G", "AUU" => "I", "AUC" => "I", "AUA" => "I", "AUG" => "M", "ACU" => "T", "ACC" => "T", "ACA" => "T", "ACG" => "T", "AAU" => "N", "AAC" => "N", "AAA" => "K", "AAG" => "K", "AGU" => "S", "AGC" => "S", "AGA" => "R", "AGG" => "R", "CUU" => "L", "CUC" => "L", "CUA" => "L", "CUG" => "L", "CCU" => "P", "CCC" => "P", "CCA" => "P", "CCG" => "P", "CAU" => "H", "CAC" => "H", "CAA" => "Q", "CAG" => "Q", "CGU" => "R", "CGC" => "R", "CGA" => "R", "CGG" => "R" 
 }
 
-seq = ARGV[0]
+seq = ARGV[0].each_char.to_a
+result = ""
+
+loop do
+  triplet = seq.slice!(3)
+  codon = CODONS.fetch(triplet.join)
+  result += codon
+end
+
+
+
